aa5816f41764440a194bd1f70db34a4bd8eb3f7a
ItsMoonlightGacha <78887101+ItsMoonlightGacha@users.noreply.github.com>
2021-10-05 22:44:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+prediction1="";
+prediction2="";
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jsHEzUfFS/model.json',modelLoaded);
+function modelLoaded() {
+    console.log('modelLoaded');
+}
+function speak() {
+    var synth=window.SpeechSynthesis;
+    speak_data_1="the first prediction is"+prediction1;
+    speak_data_2="and the second prediction is"+prediction2;
+    var utterthis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterthis);
+}
